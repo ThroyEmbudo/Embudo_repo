25ecbb3a511a01327070e2ab8c7aac160d4ac907
Your Name <throyembudo@gmail.com>
2018-02-09 23:32:15 +0800

    Quizzes

---
+++ web_112817/form.php
@@ -0,0 +1,16 @@
+<html>
+<head><title></title> </head>
+<body>
+<form action=" " method=" get">
+<p>Login</p>
+<p>Username:
+<br /> <input type=" text" name="username" /> </p>
+<p> Password:
+<br /><input type=" password" name=" password" /></p>
+<p>
+	<input type=" submit" value =" Login" />
+	<input type=" reset" value=" clear" />
+
+</form>
+</body>
+</html>
